dfa4eb3d9de07e2387fa45269b3ccac02495a014
Crowdbotics <team@crowdbotics.com>
2021-09-19 06:01:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "e_zevis_30671Identifier",
+  url: "https://e_zevis_30671Identifier.botics.co",
+  api: "https://e_zevis_30671Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
