885a3f9629ff0521716b5ce6bb33783543e78042
Crowdbotics <team@crowdbotics.com>
2021-09-19 06:25:34 +0000

    Update models

--- backend/e_zevis_30671/wsgi.py
+++ backend/e_zevis_30671/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_zevis_30671.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "e_zevis_30671.settings")
 
 application = get_wsgi_application()
